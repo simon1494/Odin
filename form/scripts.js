66511dac733e2b5ed91c0c3ad806887ea2846968
Simon Bierozko <105741786+simon1494@users.noreply.github.com>
2024-08-21 09:21:49 -0300

    asdf

--- form/scripts.js
+++ form/scripts.js
@@ -13,10 +13,24 @@ function placeMainContainer () {
     body.appendChild(container)    
 }
 
+
 function placeContainers(parent, howMany=2, clase="algo") {
    
     for (let i = 1; i < howMany+1; i++) {
         const container = document.createElement("div");
+        container.classList.add(clase)
+        container.id = `${clase}_${i}`;
+        parent.appendChild(container);
+    }
+       
+}
+
+function placeForms(parent, howMany=2, clase="algo") {
+   
+    for (let i = 1; i < howMany+1; i++) {
+        const container = document.createElement("form");
+        container.action = "https://httpbin.org/post" 
+        container.method = "POST"
         container.classList.add(clase)
         container.id = `${clase}_${i}`;
         parent.appendChild(container);
@@ -38,6 +52,8 @@ function placeIguano() {
 function placeForm(parent) {
     const form = document.createElement("form")
     form.id = "formulario"
+    form.action = "https://httpbin.org/post" 
+    form.method = "POST"
     setForm(form)
     parent.appendChild(form)
 }
@@ -73,22 +89,22 @@ function setForm() {
     createInput(telefono, "Telefono")
     createInput(pass1, "Contraseña 1")
     createInput(pass2, "Contraseña 2")
-
-
-
 }
 
 placeMainContainer()
 placeContainers(retrieveElement(".container"), howMany=3, clase="prueba")
 placeContainers(retrieveElement("#prueba_2"), howMany=2, clase="sub_prueba")
-placeContainers(retrieveElement("#sub_prueba_2"), howMany=3, clase="form")
+
+placeForms(retrieveElement("#sub_prueba_2"), howMany=3, clase="form")
 placeImage(retrieveElement("#sub_prueba_1"),"forest.avif")
 
 placeContainers(retrieveElement("#sub_prueba_1"), howMany=1, clase="banner")
 placeContainers(retrieveElement(".banner"), howMany=2, clase="sub_banner")
 placeImage(retrieveElement("#sub_banner_1"),"gecko.svg")
 placeIguano()
+placeContainers(retrieveElement("#form_1"), howMany=2, clase="sub_form")
 placeContainers(retrieveElement("#form_2"), howMany=4, clase="ingresos")
+placeContainers(retrieveElement("#form_3"), howMany=2, clase="sub_form")
 
 placeContainers(retrieveElement("#ingresos_2"), howMany=3, clase="info_nombre")
 placeContainers(retrieveElement("#ingresos_3"), howMany=3, clase="info_contacto")
@@ -96,5 +112,27 @@ placeContainers(retrieveElement("#ingresos_4"), howMany=3, clase="info_pass")
 
 setForm()
 
-const form_1 = document.querySelector("#form_1")
-form_1.textContent = "Alo"
+const form_1 = document.querySelector("#sub_form_2")
+form_1.textContent = "Iguano Love es tu plataforma favorita para encontrar tu iguano amor. Crea tu cuenta, nosotros nos encargamos del resto."
+
+function botoncito(){
+    const algo = document.querySelector("#form_3 > #sub_form_1")
+    boton = document.createElement("button")
+    boton.id = "botoncito"
+    boton.textContent = "Crear cuenta"
+    boton.form = "form_2"
+    console.log(boton)
+    algo.appendChild(boton)
+
+    document.getElementById('botoncito').addEventListener('click', function() {
+        document.getElementById('form_2').submit();
+      });
+}
+
+
+
+leyenda = document.createElement("h2")
+leyenda.id = "botoncito"
+leyenda.textContent = "Ya tenes cuenta?"
+
+botoncito()
